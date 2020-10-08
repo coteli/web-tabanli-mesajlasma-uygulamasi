9ad07f5f10e339078f05a3a31202dabd7bc2b8e8
coteli <avsarcoteli@gmail.com>
2020-10-08 11:53:26 +0300

    ilk commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAw0S_eSiC1MEqAVlpKh4NauX_Sq0owXVQ",
+    authDomain: "wtmu-a6f69.firebaseapp.com",
+    databaseURL: "https://wtmu-a6f69.firebaseio.com",
+    projectId: "wtmu-a6f69",
+    storageBucket: "wtmu-a6f69.appspot.com",
+    messagingSenderId: "452804808537",
+    appId: "1:452804808537:web:c4cc98aa08ed81f3eb0648"
+  };
